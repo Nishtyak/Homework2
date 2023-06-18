8e33c81aee004040763ed5bb9f0dffa9d39d6d05
Tatyana <KrukovaTatyana1990@gmail.com>
2023-06-18 11:15:29 +0300

    Adding check negative numbers in Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,5 +6,24 @@
 Console.Write("Введите число: ");
 string number = Console.ReadLine();
 
-if(number.Length < 3) Console.Write("Третьей цифры нет");
-else Console.Write($"{number} -> {number[2]}");
+int index = 0;
+int min_lenght = 0;
+
+void check_negative (string num, out int index, out int min_lenght)
+{
+    if(num.StartsWith("-")) 
+    {
+        index = 3;
+        min_lenght = 4;
+    }
+    else
+    {
+        index = 2;
+        min_lenght = 3;
+    }
+}
+
+check_negative(number,out index,out min_lenght);
+
+if(number.Length < min_lenght) Console.Write("Третьей цифры нет");
+else Console.Write($"{number} -> {number[index]}");
